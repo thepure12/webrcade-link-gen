5177cc150fc20468c861893692300f19c8e98067
TJ Renninger <renningert@LAPTOP-LLEJQRTO.localdomain>
2022-09-28 15:32:49 -0400

    initial

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="d-flex justify-content-center bg-black">
+            <img src="banner2.png" alt="">
+        </div>
+        <div class="container py-3">
+            <b-card>
+                <Nuxt />
+            </b-card>
+        </div>
+    </div>
+</template>
+<style>
+.bg-black {
+    background-color: black;
+}
+</style>
